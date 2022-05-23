9c9ba9a887b0abcf41722dcb2eaa7ec6171872ab
nguyentung3107 <91296928+nguyentung3107@users.noreply.github.com>
2022-05-24 05:12:55 +0700

    Special for u

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Cậu chưa chào lại tớ hả 😠  `,
+    btnIntro: 'Chào 😓 ',
+    title: 'Tớ biết cậu có nhiều điều muốn nói với tớ phải hong nè ? 😙',
+    desc: 'Chắc là do cậu hâm mộ tớ quá hả 😳  ',
+    btnYes: 'Đúng rồi đó, tớ hâm hộ cậu thực sự luônnn',
+    btnNo: ' Đâu, đâu làm gì có 🤨 ',
+    question:'Nói cho tớ biết đi, vì sao cậu lại hâm mộ tớ thế 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì cậu đẹp trai nè, nhiều tài năng nè Ahiiiii 😊😊',
+    mess: 'Hì hì, Đùa cậu một chút thôi. Thực ra đây là chỗ để cậu viết lưu bút cho tớ nè 😘😘',
+    messDesc: '💬💬 Viết cho tớ vài dòng nha 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://sprw.io/stt-b6923b6fb0'
+}
